55158b354697e5351fb4bb3883f5e7fed6a71837
Harsh Gupta <140882331+Harshibmx65@users.noreply.github.com>
2024-09-07 17:59:29 +0530

    Create 4.r

---
+++ 4.r
@@ -0,0 +1,14 @@
+num1=5
+num2=3
+
+# cat(num1,file="arithmetic.txt")
+# getwd()
+
+cat(num1==num2,"\n",
+    num1!=num2,"\n",
+    num1<num2,"\n",
+    num1>num2,"\n",
+    num1<=num2,"\n",
+    num1>=num2,"\n",file="relate.txt")
+
+# using multiple cat will paste to file the last result only
