907e55a99c21594389fc39490fcb388303e8dae2
Harsh Gupta <140882331+Harshibmx65@users.noreply.github.com>
2024-09-07 17:58:59 +0530

    Create 3.r

---
+++ 3.r
@@ -0,0 +1,23 @@
+{
+num1=as.integer(readline())
+num2=as.integer(readline())
+
+sum=num1+num2
+dif=num1-num2
+mul=num1*num2
+div=num1/num2
+rem=num1%%num2
+pow=num1^num2
+
+cat("sum:",sum,"\n")
+cat("difference:",dif,"\n")
+cat("multiplication:",mul,"\n")
+cat("division:",div,"\n")
+cat("Remainder:",rem,"\n")
+cat("Power:",pow,"\n")
+
+
+}
+# paste can be used too
+# modulus- (%%)
+# power- ^
