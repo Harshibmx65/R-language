174c684cf40c66cca5d8860e46b31e52b1432324
Harsh Gupta <140882331+Harshibmx65@users.noreply.github.com>
2024-09-07 18:00:00 +0530

    Create 5.r

---
+++ 5.r
@@ -0,0 +1,17 @@
+x="hello"
+y="students"
+cat(x,y,sep=" happy ")
+
+cat(x,y,file="demo.txt",sep=" happy ")
+#if file already exists then it will replace the whole content
+
+
+getwd()
+
+?write()
+
+a=5
+class(a)
+
+a=10.5L
+class(a)
